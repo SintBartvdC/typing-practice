657aba21a4033cd4869ad991f02d4c44189d6b9e
Bart <b.van.de.crommert@sintlucasedu.nl>
2023-08-30 21:50:55 +0200

    .

--- js/main.js
+++ js/main.js
@@ -6,3 +6,86 @@ const verhalen = [
     "Op een dag kreeg de verlegen Mia een mysterieus pakket. In het pakket zat een antiek zakhorloge met een gravure erop: De tijd onthult geheimen. Nieuwsgierig besloot Mia om het horloge om haar pols te dragen. Tot haar verbazing begon het horloge plotseling te tikken en licht uit te stralen. Wanneer Mia het horloge vasthield, werd ze getransporteerd naar verschillende momenten in de geschiedenis. Ze beleefde avonturen met piraten, ontmoette beroemde kunstenaars, en stond oog in oog met dinosaurussen. Met elk tijdreisavontuur groeide Mia in zelfvertrouwen en leerde ze veel over de wereld en zichzelf. Uiteindelijk ontdekte Mia het geheim van het horloge: het toonde haar de geschiedenis en bracht lessen over het belang van tijd en momenten in het leven. Na vele avonturen besloot Mia het horloge te delen met anderen, zodat ook zij hun eigen waardevolle lessen konden leren.",
     "In een klein dorpje woonde een meisje genaamd Lily, die dol was op zeepbellen. Elke dag blies ze zeepbellen in alle kleuren van de regenboog. Op een bijzondere dag gebeurde er iets magisch: een van de zeepbellen kwam tot leven en begon te groeien! Lily volgde de vliegende zeepbel, die haar meenam op een reis hoog in de lucht. Ze ontmoette wolkenwezens, dansende vogels en vliegende vissen. Samen verkenden ze fantastische plekken die niemand ooit had gezien. Na een betoverend avontuur bracht de zeepbel Lily veilig terug naar huis. Ze besefte dat de magie altijd aanwezig was, zelfs in de eenvoudigste dingen. Vanaf die dag deelde Lily haar zeepbellen met andere kinderen in het dorp, waardoor ze allemaal een beetje van de magie konden ervaren die ze in de vliegende zeepbel had ontdekt.",
 ];
+
+const typingText = document.getElementById("verhalen")
+const inpField = document.querySelector(".wrapper .input-field")
+const tryAgainBtn = document.querySelector(".content button")
+const timeTag = document.querySelector(".time span b")
+const mistakeTag = document.querySelector(".mistake span")
+
+let timer;
+let maxTime = 60;
+let timeLeft = maxTime;
+let charIndex = mistakes = isTyping = 0;
+
+// random inladen van verhalen
+function loadParagraph() {
+    const ranIndex = Math.floor(Math.random() * verhalen.length);
+    typingText.innerHTML = "";
+    verhalen[ranIndex].split("").forEach(char => {
+        let span = `<span>${char}</span>`
+        typingText.innerHTML += span;
+    });
+    typingText.querySelectorAll("span")[0].classList.add("active");
+    inpField.focus();
+}
+
+// registreren of persoon aan het typen is en alle fouten registreren
+function initTyping() {
+    let characters = typingText.querySelectorAll("span");
+    let typedChar = inpField.value.split("")[charIndex];
+    if (charIndex < characters.length - 1 && timeLeft > 0) {
+        if (!isTyping) {
+            timer = setInterval(initTimer, 1000);
+            isTyping = true;
+        }
+        if (typedChar == null) {
+            if (charIndex > 0) {
+                charIndex--;
+                if (characters[charIndex].classList.contains("incorrect")) {
+                    mistakes--;
+                }
+                characters[charIndex].classList.remove("correct", "incorrect");
+            }
+        } else {
+            if (characters[charIndex].innerText == typedChar) {
+                characters[charIndex].classList.add("correct");
+            } else {
+                mistakes++;
+                characters[charIndex].classList.add("incorrect");
+            }
+            charIndex++;
+        }
+
+        mistakeTag.innerText = mistakes;
+    } else {
+        clearInterval(timer);
+        inpField.value = "";
+    }
+}
+
+// zorgen dat de timer aftelt
+function initTimer() {
+    if (timeLeft > 0) {
+        timeLeft--;
+        timeTag.innerText = timeLeft;
+    } else {
+        clearInterval(timer);
+    }
+}
+
+// zorgen dat de game wordt gereset 
+function resetGame() {
+    loadParagraph();
+    clearInterval(timer);
+    timeLeft = maxTime;
+    charIndex = mistakes = isTyping = 0;
+    inpField.value = "";
+    timeTag.innerText = timeLeft;
+    mistakeTag.innerText = 0;
+}
+
+// ervoor zorgen dat er verhalen kunnen worden ingeladen
+loadParagraph();
+inpField.addEventListener("input", initTyping);
+tryAgainBtn.addEventListener("click", resetGame);
